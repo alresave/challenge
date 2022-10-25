dc737a2b65cb3722394c758ada149ef5322dc1c0
alresave <alresave@gmail.com>
2022-10-24 22:32:23 -0500

    Added common structs

--- common/user/user.go
+++ common/user/user.go
@@ -1,7 +1,14 @@
 package user
 
 type Info struct {
-	Id    int
+	Id    int    `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
+}
+
+type User struct {
+	Id       int    `json:"id"`
+	UserName string `json:"userName"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
